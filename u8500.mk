c41aee9896a517c61e42a094a1d5fd8fb8b7f48c
Shilin Victor <chrono.monochrome@gmail.com>
2016-05-06 14:05:33 +0700

    Disable libUMP build for now
    Change-Id: I778c454a98a57c06a92d4422e4aba73e68c2f994

--- u8500.mk
+++ u8500.mk
@@ -6,7 +6,6 @@ $(call inherit-product, hardware/u8500/audio/libasound/alsa-lib-products.mk)
 # STE Display 
 PRODUCT_PACKAGES += \
    lights.montblanc \
-   libUMP \
    hwcomposer.montblanc \
    copybit.montblanc \
    libblt_hw
