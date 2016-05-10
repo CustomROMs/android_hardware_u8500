09502d714e01f9a56ae9312208ad38f09336e280
Shilin Victor <chrono.monochrome@gmail.com>
2016-05-10 15:12:10 +0700

    Add benchmark
    Change-Id: I66c1c25ddee2f9d357a8302e931b16f391c83879

--- tests/Android.mk
+++ tests/Android.mk
@@ -10,9 +10,20 @@ LOCAL_CFLAGS += -fno-strict-aliasing -fno-common -Werror-implicit-function-decla
 LOCAL_SRC_FILES := whetstone.c
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := optional
-LOCAL_STATIC_LIBRARIES := libm
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libm
 LOCAL_MODULE := whetstone
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := $(COMMON_FLAGS) -O2 -Wall -Wundef
+LOCAL_CFLAGS += -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration
+#LOCAL_LDFLAGS := -Wl,--no-gc-sections
+LOCAL_SRC_FILES := whetstone.c
+LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
+LOCAL_MODULE_TAGS := optional
+LOCAL_SYSTEM_SHARED_LIBRARIES := libc libm
+LOCAL_MODULE := whetstone_thumb
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
---
+++ tests/coremark/clean.sh
@@ -0,0 +1,1 @@
+rm -fr ~/repo/out/target/product/codina/obj/EXECUTABLES/coremark_*
