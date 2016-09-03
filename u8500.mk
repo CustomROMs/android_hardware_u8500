9578d6bca668b97dbf650cfe55da05a9c65535b7
Shilin Victor <chrono.monochrome@gmail.com>
2016-09-03 23:15:16 +0700

    Disable build of some libs for now
    Change-Id: Id5ef3a7b31098fbc859dabbd28f6af715b78a584

--- u8500.mk
+++ u8500.mk
@@ -1,23 +1,25 @@
 # Audio
-PRODUCT_PACKAGES += \
-   libasound
-$(call inherit-product, hardware/u8500/audio/libasound/alsa-lib-products.mk)
+#PRODUCT_PACKAGES += \
+#   libasound
+#$(call inherit-product, hardware/u8500/audio/libasound/alsa-lib-products.mk)
 
 # STE Display 
 PRODUCT_PACKAGES += \
-   lights.montblanc \
-   hwcomposer.montblanc \
-   copybit.montblanc \
-   libblt_hw
+   lights.montblanc
+
+#   hwcomposer.montblanc \
+#   copybit.montblanc \
+#   libblt_hw
 
 # STE Media
 PRODUCT_PACKAGES += \
-   libomxil-bellagio \
-   libstelpcutils \
-   libstagefrighthw \
-   libstagefright_soft_ste_wmapro_v10 \
-   libstagefright_soft_ste_mp3dec \
-   libstagefright_soft_ste_aacdec
+   libomxil-bellagio
+
+#   libstelpcutils \
+#   libstagefrighthw \
+#   libstagefright_soft_ste_wmapro_v10 \
+#   libstagefright_soft_ste_mp3dec \
+#   libstagefright_soft_ste_aacdec
 
 # WLAN
 PRODUCT_PACKAGES += \
