0306802278449a19068b8f03210f7e97233eef2e
Shilin Victor <chrono.monochrome@gmail.com>
2017-10-29 02:39:21 +0300

    remove libcamera

--- libcamera/string11.h
+++
@@ -1,5 +0,0 @@
-//typedef int size_t;
-extern "C" char* strerror(int);
-extern "C" int strlen(const char *);
-extern "C" int    strcmp(const char *, const char *);
-extern "C" char* strcpy(char* dest, const char* src);
