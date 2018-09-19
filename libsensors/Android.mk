fe5477df074fcac84d9a01d25819c19c0158ccb1
Shilin Victor <chrono.monochrome@gmail.com>
2018-09-19 04:55:29 +0300

    libsensors: add HAL
    import from "8af87ba libsensors: only build for STE hardware"

---
+++ libsensors/Android.mk
@@ -0,0 +1,23 @@
+ifeq ($(BOARD_USES_STE_HARDWARE),true)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -std=gnu99
+
+LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_CFLAGS := -std=gnu99
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/lib/hw
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils
+
+LOCAL_SRC_FILES := sensors.c
+
+include $(BUILD_SHARED_LIBRARY)
+endif
