8f90eb34cfe412f10f482e3126ee47c260c0b16a
Shilin Victor <chrono.monochrome@gmail.com>
2019-03-11 16:20:57 +0300

    Revert "Use sw_vsync implementation by Texas Instruments"
    This reverts commit 3db2162ccb53a465c74ddcd87adba65d54a85247.
    
    Change-Id: Ib191b01b3f93e0e2ee0ff72b469e26310ae7162c

--- display/libhwcomposer/vsync_monitor.h
+++ display/libhwcomposer/vsync_monitor.h
@@ -10,17 +10,12 @@
 #define VSYNC_MONITOR_H (1)
 
 #include "hardware/hwcomposer.h"
-//#include "hwc_dev.h"
 
-typedef struct vsync_monitor_context vsync_monitor_context_t;
+int vsync_monitor_init(int compdev_fd);
 
-//int vsync_monitor_init(int compdev_fd);
-void vsync_monitor_init(vsync_monitor_context_t *hwc_dev);
+int vsync_monitor_enable(struct hwc_procs* procs);
 
-void vsync_monitor_enable();
-//int vsync_monitor_enable(struct hwc_procs* procs);
-
-void vsync_monitor_disable();
+int vsync_monitor_disable();
 
 void vsync_monitor_destroy();
 
