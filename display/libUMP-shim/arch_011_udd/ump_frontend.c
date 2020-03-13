1842930f0022a150f86eaabdce4ffad155f121b9
Shilin Victor <chrono.monochrome@gmail.com>
2020-03-13 15:03:50 +0300

    UMP: add copyrights
    Change-Id: I0c5cf34a3648ef31d5e48eab048c1683f4869975

--- display/libUMP-shim/arch_011_udd/ump_frontend.c
+++ display/libUMP-shim/arch_011_udd/ump_frontend.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2010-2011, 2013 ARM Limited. All rights reserved.
  *
+ *           (C) 2020 Shilin Victor <chrono.monochrome@gmail.com>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
